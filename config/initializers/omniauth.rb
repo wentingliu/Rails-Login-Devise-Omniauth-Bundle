439558963b65d95800a8a8c766750749596bb51d
Çağdaş <cagdas@cloudarena.com>
2012-04-09 10:41:29 +0300

    Update config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'AMIQ7h9ZlyA3KuNI3r0EQ', 'RVnsq8nRNTBOZgG5sU4hsWAm6SnuLeXibF26xlhVBQ'
-  provider :facebook, '328360657225453', 'd741f93ada5252de560c9dad8bc771df'
-  provider :google_oauth2, '438252874224-mm2l0qbkdd6v75jfaj9mi5rcdu6n2ie1.apps.googleusercontent.com', 'o2MvGW-Ne-xfyJWq5vxJo6If'
+  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :facebook, 'KEY', 'SECRET'
+  provider :google_oauth2, 'CLIENT_ID', 'CLIENT_SECRET'
 end
